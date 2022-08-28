7f85773d2e165fe2517e033d658df3d236269d44
pepprbell <pepprbell@gmail.com>
2022-08-28 22:01:34 +0900

    Add operate function

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-function add(a,b) {
-  return a + b
+let priority = {'+':1, '-':1, '×':2, '÷':2}
+
+function operate() {
+  let infix = document.querySelector('p[data-key=equation]').innerHTML.trim().split(' ')
+  let postfix = convertInfix(infix)
+
+
+  if (infix.length === 1) {
+    return
+  }
+
+  let res = calculate(postfix)
+  curr.innerHTML = res
 }
 
-function subtract(a,b) {
-  return a - b
-}
+function convertInfix(infix) {
+  let stack = []
+  let postfix = []
 
-function multiply(a,b) {
-  return a * b
-}
+  for (let x = 0; x < infix.length; x++) {
+    let i = infix[x]
+    if (i[0].match(/^[0-9]+$/)) {
+      postfix.push(i)
+      continue
+    }
+    while (stack.length >= 1) {
+      let last = stack[stack.length-1]
+      if (priority[last] < priority[i]) {
+        break
+      }
+      postfix.push(stack.pop())
+    }
+    stack.push(i)        
+  }
+  while (stack.length >= 1) {
+    postfix.push(stack.pop())
+  }
 
-function divide(a,b) {
-  return a / b
+  return postfix
 }
