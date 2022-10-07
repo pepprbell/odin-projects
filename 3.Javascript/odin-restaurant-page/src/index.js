6c0cdf622b9d66c6a8dbe5a8a9faa14eae7b6e14
pepprbell <pepprbell@gmail.com>
2022-10-07 17:01:50 +0900

    Add functions for reservation modal

--- 3.Javascript/odin-restaurant-page/src/index.js
+++ 3.Javascript/odin-restaurant-page/src/index.js
@@ -1,1 +1,22 @@
-console.log(1)
+const reservation = document.querySelector('calendar-button')
+
+function reserve(e) {
+  const modal = document.querySelector('.modal')
+  e.target.disabled = true
+  setTimeout(() => {
+    e.target.disabled = false
+  }, 2400);
+
+  modal.classList.add('modal-on')
+
+  const modalTimer = () => {
+    modal.classList.add('modal-timer')
+    setTimeout(modalOff, 2000)
+  }
+
+  const modalOff = () => {
+    modal.classList.remove('modal-on')
+    modal.classList.remove('modal-timer')
+  }
+  setTimeout(modalTimer, 200)
+}
