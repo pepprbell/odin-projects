3ea9c83c6ada9e42b4803e832624d930471545ce
pepprbell <pepprbell@gmail.com>
2023-01-16 13:33:12 +0900

    Add html components and css for google icon

---
+++ 3.Javascript/odin-weather-app/index.js
@@ -0,0 +1,12 @@
+const key = '855ca86de94d46e59cd4dd9314b422d1'
+
+function getGeocoding() {
+  // 엔터쳤을 때 불러와져서 input값 바탕으로 검색 돌리기
+  // return lat, lon
+}
+
+function getWeather() {
+  // getGeocoding에서 lat, lon 받아와서 fetch
+  // 로딩하는동안 하얀/까만 반투명 띄우기?
+  // 데이터 받아와서 표시
+}
