8afecb0f49a89569e90360e271af221ac196d327
pepprbell <pepprbell@gmail.com>
2023-02-01 12:10:48 +0900

    Add test code

--- Mini Projects/binary-search-trees/test.js
+++ Mini Projects/binary-search-trees/test.js
@@ -1,64 +1,49 @@
 let arr = [];
-// for (let i = 1; i < 5; i++) {
-//   let next = Math.round(Math.random()*100)
-//   arr.push(next)  
+// for (let i = 1; i < 12; i++) {
+//   arr.push(i*10)  
 // }
-for (let i = 1; i < 7; i++) {
-  arr.push(i+1)  
+for (let i = 4; i < 15; i++) {
+  arr.push(i)  
 }
 console.log(`arr = [${arr}]`)
 console.log(`tree`)
 
 // create tree
 let thisTree = tree(arr)
-console.log(`test find ->`, thisTree.find(3))
+console.log(`Is this tree balanced? `,thisTree.isBalanced())
 
-// Confirm that the tree is balanced
-console.log(thisTree.isBalanced())
+// find one node, and check it's height and depth
+let n = arr[Math.floor(Math.random()*arr.length)]
+console.log(`find node ${n} ->`, thisTree.find(n))
+let findNode = thisTree.find(n)
+console.log(`node ${n}'s height ->`, thisTree.height(findNode))
+console.log(`node ${n}'s depth ->`, thisTree.depth(findNode))
 
 // Print out all elements in level, pre, post, and in order
-console.log(thisTree.levelOrder())
-console.log(thisTree.preorder())
-console.log(thisTree.postorder())
-console.log(thisTree.inorder())
+console.log('level order:',thisTree.levelOrder())
+console.log('preorder:', thisTree.preorder())
+console.log('postorder:', thisTree.postorder())
+console.log('inorder: ',thisTree.inorder())
 
-
-
-// class Node
-// {
-//     constructor(d)
-//     {
-//         this.data = d;
-//         this.left = null;
-//         this.right = null;
-//     }
+console.log('------------------------')
+// add some numbers
+// for (let i = 1; i < 20; i++) {
+//   thisTree.insert(Math.round(Math.random()*36))
 // }
- 
-// let root = null;
- 
-// /* A function that constructs Balanced Binary Search Tree 
-//  from a sorted array */
-// function sortedArrayToBST(arr, start, end)
-// {
-//     /* Base Case */
-//     if (start > end)
-//     {
-//         return null;
-//     }
-//     /* Get the middle element and make it root */
-//     var mid = parseInt((start + end) / 2);
-//     var node = new Node(arr[mid]);
-//     /* Recursively construct the left subtree and make it
-//      left child of root */
-//     node.left = sortedArrayToBST(arr, start, mid - 1);
-//     /* Recursively construct the right subtree and make it
-//      right child of root */
-//     node.right = sortedArrayToBST(arr, mid + 1, end);
-//     return node;
-// }
- 
- 
-// var n = arr.length;
-// root = sortedArrayToBST(arr, 0, n - 1);
-// console.log('ex')
-// prettyPrint(root)
+thisTree.insert(0)
+thisTree.insert(1)
+thisTree.insert(2)
+thisTree.insert(3)
+prettyPrint(thisTree.getRoot())
+
+// Confirm that the tree is balanced
+console.log(`Is this tree balanced? `, thisTree.isBalanced())
+thisTree.rebalance()
+console.log(`Rebalance succeded? `, thisTree.isBalanced())
+prettyPrint(thisTree.getRoot())
+
+let m = arr[Math.floor(Math.random()*arr.length)]
+
+console.log('remove',m)
+thisTree.remove(m)
+prettyPrint(thisTree.getRoot())
