d459c5e89eb038c2e2180024d282fc65869e1708
pepprbell <pepprbell@gmail.com>
2023-02-08 16:54:57 +0900

    Add player factory and ai using random coordinate picks

---
+++ 3.Javascript/odin-battleship/factories/playerFactory.js
@@ -0,0 +1,16 @@
+function Player(ai = false) {
+  const visited = []
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      visited.push([i,j])
+    }
+  }
+  return {
+    pick: function() {
+      const nextCoord = visited.splice(Math.floor(Math.random()*visited.length), 1) 
+      return nextCoord
+    }
+  }
+}
+
+export default Player
