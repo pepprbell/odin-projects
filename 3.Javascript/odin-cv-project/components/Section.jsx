c87d93e8e47d5f0b8f81a7b6382216d13c4f04a1
pepprbell <pepprbell@gmail.com>
2023-02-20 16:50:40 +0900

    Add default template for project, create 3 components (header/input/section)

---
+++ 3.Javascript/odin-cv-project/components/Section.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Section extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
