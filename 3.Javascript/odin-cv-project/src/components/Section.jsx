08c2c5b5d47ec0f319dcaa8454267f65b03ca68b
pepprbell <pepprbell@gmail.com>
2023-02-22 14:34:19 +0900

    Refactor Section.jsx into functional component

--- 3.Javascript/odin-cv-project/src/components/Section.jsx
+++ 3.Javascript/odin-cv-project/src/components/Section.jsx
@@ -1,57 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Input from './Input'
 import '../styles/Section.css'
 
-class Section extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isEditing: false
-    }
+function Section(props) {
 
-    this.handleClick = this.handleClick.bind(this)
+  const [isEditing, setIsEditing] = useState(false)
+
+  function handleClick() {
+    setIsEditing(!isEditing)
   }
 
-  handleClick() {
-    this.setState(prevState => ({
-      isEditing: !prevState.isEditing
-    }))
-  }
-
-  render() {
-    const data = this.props.data.data
-    const items = data.map((item, idx) =>
-       <div className='container' key={idx} id={this.props.data.sectionName + idx}>
-        <div className='left'>
-          <Input type='date' isEditing={this.state.isEditing}>{item.date}</Input>
+  const data = props.data.data
+  const items = data.map((item, idx) =>
+      <div className='container' key={idx} id={props.data.sectionName + idx}>
+      <div className='left'>
+        <Input type='date' isEditing={isEditing}>{item.date}</Input>
+      </div>
+      <div className='v-line'></div>
+      <div className='right'>
+        <div>
+          <Input type='title' isEditing={isEditing}>{item.title}</Input> 
+          <Input type='desc' isEditing={isEditing}>{item.desc}</Input>
         </div>
-        <div className='v-line'></div>
-        <div className='right'>
-          <div>
-            <Input type='title' isEditing={this.state.isEditing}>{item.title}</Input> 
-            <Input type='desc' isEditing={this.state.isEditing}>{item.desc}</Input>
-          </div>
-        </div>
-       </div> 
-      )
-
-    let button;
-    if (!this.state.isEditing) {
-      button = <button onClick={this.handleClick}><span className="material-symbols-outlined">edit</span></button>
-    } else {
-      button = <button onClick={this.handleClick}><span className="material-symbols-outlined">done</span></button>
-    }
-      
-    return (
-      <div className='section-body'>
-        <Input type='section'>{this.props.data.sectionName}</Input>
-        {button}
-        <div className='section-list'>
-          {items}
-        </div>
-    </div>
+      </div>
+      </div> 
     )
+
+  let button;
+  if (!isEditing) {
+    button = <button onClick={handleClick}><span className="material-symbols-outlined">edit</span></button>
+  } else {
+    button = <button onClick={handleClick}><span className="material-symbols-outlined">done</span></button>
   }
+      
+  return (
+    <div className='section-body'>
+      <Input type='section'>{props.data.sectionName}</Input>
+      {button}
+      <div className='section-list'>
+        {items}
+      </div>
+  </div>
+  )
 }
 
 export default Section
