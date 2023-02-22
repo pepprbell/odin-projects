647dd6627c79ba952961cd0a217b90d635277db2
pepprbell <pepprbell@gmail.com>
2023-02-22 14:31:51 +0900

    Refactor Header.jsx into functional component

--- 3.Javascript/odin-cv-project/src/components/Header.jsx
+++ 3.Javascript/odin-cv-project/src/components/Header.jsx
@@ -1,20 +1,16 @@
-import React, { Component } from 'react'
 import '../styles/Header.css'
 
-class Header extends Component {
-  link = 'https://www.' + this.props.data.github
+function Header(props) {
+  const link = 'https://www.' + props.data.github
 
-  render() {
-    console.log(this.props)
-    return (
-      <div className='header'>
-        <h2>{this.props.data.name}</h2>
-        <p>{this.props.data.email}</p>
-        <a href={this.link}>{this.props.data.github}</a>
-        <div className='line'></div>
-      </div>
-    )
-  }
+  return (
+    <div className='header'>
+      <h2>{props.data.name}</h2>
+      <p>{props.data.email}</p>
+      <a href={link}>{props.data.github}</a>
+      <div className='line'></div>
+    </div>
+  )
 }
 
 export default Header
