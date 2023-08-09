62c0aa86ed53684d4b41b6775e4dcaabc53d7151
pepprbell <pepprbell@gmail.com>
2023-08-09 12:02:40 +0900

    Add fetchData function that fetch data

---
+++ 5.React/odin-shopping-cart/src/hooks/fetchData.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const fetchData = (category) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.nookipedia.com/nh/" + category, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': null,
+      }
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setData(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { data, error, loading };
+};
+
+export default fetchData;
