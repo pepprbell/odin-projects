71936411f8c5bbc3adc98c3363ca1f91ecc27b2f
pepprbell <pepprbell@gmail.com>
2023-08-18 11:36:47 +0900

    Adjust router

--- 5.React/odin-shopping-cart/src/Router.jsx
+++ 5.React/odin-shopping-cart/src/Router.jsx
@@ -3,14 +3,32 @@ import ErrorPage from "./pages/ErrorPage";
 import Home from './pages/Home'
 import Category from './pages/Category'
 import Cart from './pages/Cart'
+import Details from './pages/Details'
 import App from "./App";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      // path: ":page",
       element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: 'cart',
+          element: <Cart />
+        },
+        {
+          path: ':type',
+          element: <Category />,
+        },
+        {
+          path: ':type/:name',
+          element: <Details />
+        }
+      ],
       errorElement: <ErrorPage />,
     },
   ])
