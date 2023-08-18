7bc57e6fc0e57afd1d0430888cad82012425e26d
pepprbell <pepprbell@gmail.com>
2023-08-18 11:33:23 +0900

    Fix addToCart logic since the hook(useCart) changed

--- 5.React/odin-shopping-cart/src/components/Card.jsx
+++ 5.React/odin-shopping-cart/src/components/Card.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from 'react';
 import './Card.css'
+import useCart from '../hooks/useCart';
 
-const Card = ({ res, onclick }) => {
+const Card = ({res, cartHandler}) => {
   const data = res
   const ref = useRef(0)
 
@@ -10,23 +11,13 @@ const Card = ({ res, onclick }) => {
   }, [])
 
   const addToCart = () => {
-    const id = ref.current
-
-    if (localStorage.getItem(id) === null) {
-      localStorage.setItem(id, '1')
-      alert('장바구니에 상품이 담겼습니다.')
-    } else {
-      localStorage.setItem(id, parseInt(localStorage.getItem(id)) + 1)
-      alert(`한 번 더 담으셨네요!\n장바구니 수량이 ${localStorage.getItem(id)}개가 되었습니다.`)
-    }
-
-    onclick()
+    useCart(cartHandler).addItem(res)
   }
 
   return (
     <div className="card">
       <figure>
-        {data['image_url'] != '' ? <img src={data['image_url']} alt=""  /> : <span className="material-symbols-outlined">image_not_supported</span> }
+        {data['image_url'] != '' ? <img src={data['image_url']} alt=""  /> : <div><span className="material-symbols-outlined notSupported">image_not_supported</span></div> }
         <button onClick={addToCart}><span className="material-symbols-outlined">add_shopping_cart</span></button>
       </figure>
       <p>{data.name}</p>
