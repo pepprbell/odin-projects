cea31a9fa5b119ce936ea9956d2522c1e8c9d288
pepprbell <pepprbell@gmail.com>
2023-08-18 11:37:50 +0900

    Fix logic - changed data storage method, from props to context. Currently using Context.Provider for this

--- 5.React/odin-shopping-cart/src/App.jsx
+++ 5.React/odin-shopping-cart/src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from 'react'
-import { useParams, Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Cart from './pages/Cart'
-import Category from './pages/Cart'
-import Home from './pages/Cart'
 import './App.css'
+import { DataContext } from './context/DataContext'
 
 function App() {
-  const [cartLength, setCartLength] = useState()
+  const [data, setData] = useState(new Map())
+  const [cart, setCart] = useState(new Map())
 
-  const updateCart = () => {
-    setCartLength(localStorage.length)
-  }
+  useEffect(() => {
+    console.log('data:', data)
+    console.log('cart:', cart)
+  }, [data, cart])
 
   return (
     <>
-      <Header cartLength={cartLength} />
-      <main className='container' >
-        <Outlet context={updateCart} />
+      <Header cartLength={cart.size} />
+      <main className='container'>
+        <DataContext.Provider value={ [[data, setData], [cart, setCart]] }>
+          <Outlet />
+        </DataContext.Provider>
       </main>
     </>
   )
