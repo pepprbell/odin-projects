d5614ce94f08f073002a19fb69733e4d59a40285
pepprbell <pepprbell@gmail.com>
2023-09-04 21:48:56 +0900

    Add card a link (to detail page)

--- 5.React/odin-shopping-cart/src/components/Card.jsx
+++ 5.React/odin-shopping-cart/src/components/Card.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useContext } from 'react';
-import nameData from '../assets/nameData'
+import { Link } from 'react-router-dom';
 import './Card.css'
 import useCart from '../utils/useCart';
 import { DataContext } from '../context/DataContext';
 
-const Card = ({ res }) => {
+const Card = ({ res, type }) => {
   const [dataHandler, cartHandler] = useContext(DataContext)
   const data = res
   const ref = useRef(0)
@@ -19,6 +19,7 @@ const Card = ({ res }) => {
 
   return (
     <div className="card">
+      <Link to={'/'+type+'/'+data.name} className='overlayLink'></Link>
       <figure>
         {data['image_url'] != '' ? <img src={data['image_url']} alt=""  /> : <div><span className="material-symbols-outlined notSupported">image_not_supported</span></div> }
         <button onClick={addToCart}><span className="material-symbols-outlined">add_shopping_cart</span></button>
