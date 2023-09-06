395388fa209e913812af25351260f6f730b3a11d
pepprbell <pepprbell@gmail.com>
2023-09-06 14:18:53 +0900

    Seperate countbutton & adjust for dynamic design

--- 5.React/odin-shopping-cart/src/components/CartObject.jsx
+++ 5.React/odin-shopping-cart/src/components/CartObject.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState, useContext } from 'react';
 import { DataContext } from '../context/DataContext';
 import './CartObject.css'
 import useCart from '../utils/useCart';
+import CountButton from './CountButton';
 
 const CartObject = ({ data, oncheck, checkRef, del }) => {
   const [dataHandler, cartHandler] = useContext(DataContext)
@@ -33,60 +34,24 @@ const CartObject = ({ data, oncheck, checkRef, del }) => {
     setChecked(newChecked)
   }
 
-  const handleInput = (e) => {
-    if (e.target.value === "") {
-      setNum(1)
-      if (e.type == 'blur') {
-        e.target.value = 1
-      }
-      setTotalCost(defaultCost.toLocaleString('en-US'))
-      return
-    }
-    const n = parseInt(e.target.value)
-    if (n > 99) {
-      alert('최대 구매 가능 개수는 99개입니다.')
-      setNum(99)
-    } else if (n <= 1) {
-      setNum(1)
-    } else {
-      setNum(n)
-    }
-  }
-
-  const handlePlus = () => {
-    if (num < 99) {
-      setNum(num+1)
-    } else {
-      alert('최대 구매 가능 개수는 99개입니다.')
-      setNum(99)
-    }
-  }
-  const handleMinus = () => {
-    if (num > 1) {
-      setNum(num-1)
-    }
-  }
-
   return (
     <menu className="cartObject">
-      <li className="cartDesc">
+      <li className='cartImg'>
         <label className='checkbox'>
-          <input type="checkbox" name="" id="" onChange={handleCheck} ref={checkRef} />
-          <span><span className="material-symbols-outlined">done</span></span>
-        </label>
-        <img src={data[0].image_url} alt="" />
-        <span className='cartProduct'>
+            <input type="checkbox" name="" id="" onChange={handleCheck} ref={checkRef} />
+            <span><span className="material-symbols-outlined">done</span></span>
+          </label>
+          <img src={data[0].image_url} alt="" />
+      </li>
+      <li className="cartDesc">
+        <span>
           <b>{data[0].categoryKR}</b>
           <p>{data[0].nameKR}</p>
         </span>
         <button className='delete' onClick={() => del(data[0])}><span></span></button>
       </li>
-      <li className="cartAmount">
-        <span>
-          <button onClick={handleMinus}><span className="material-symbols-outlined">remove</span></button>
-          <input type="number" onChange={(e) => handleInput(e)} ref={inputRef} onBlur={handleInput} min={1} max={99}/>
-          <button onClick={handlePlus}><span className="material-symbols-outlined">add</span></button>
-        </span>
+      <li className='cartAmount'>
+        <CountButton numstate={[num, setNum]} inputref={inputRef} />
       </li>
       <li className="cartCost">
         <p className="money"><b>{totalCost}</b>벨</p>
