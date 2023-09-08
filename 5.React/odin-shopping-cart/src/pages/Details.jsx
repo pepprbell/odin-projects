2ec84ec8a5adfadde7f7357e1ad28404a9774b3c
pepprbell <pepprbell@gmail.com>
2023-09-08 12:58:33 +0900

    Add styles and data to detail page

--- 5.React/odin-shopping-cart/src/pages/Details.jsx
+++ 5.React/odin-shopping-cart/src/pages/Details.jsx
@@ -1,22 +1,55 @@
+import { useEffect, useState, useContext, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import './Details.css'
 import useItemFetching from '../hooks/useItemFetching';
-import { useEffect, useState } from 'react';
+import { DataContext } from '../context/DataContext';
 import capitalize from '../utils/capitalize';
 import nameData from '../assets/nameData'
+import useCart from '../utils/useCart';
+import CountButton from '../components/CountButton'
 
 const Details = () => {
+  const [dataHandler, cartHandler] = useContext(DataContext)
   const { type, name } = useParams()
-
+  const [num, setNum] = useState(1)
+  
   const { data, error, loading } = useItemFetching(type, name)
+  
+  const inputref = useRef(null)
 
-  console.log(data)
-  console.log()
+  useEffect(() => {
+    // inputref.current.value = 1
+  }, [])
+
+  useEffect(() => {
+    inputref.current.value = num
+  }, [num])
+  
+  function handleClick() {
+    useCart('update', data[0], cartHandler, num)
+  }
 
   return (
     <section className="details">
       {loading ? (
-        <>Loading</>
+        <>
+        <aside className='detailLoading'>
+          <figure></figure>
+        </aside>
+        <article className='detailLoading'>
+          <p className='type'></p>
+          <p className='itemName'></p>
+          <i></i>
+          <q></q>
+          <p className='money'><b></b></p>
+          <p className='shippingDesc'></p>
+          <div className='option'>
+            <CountButton className="cntbutton" numstate={[num, setNum]} inputref={inputref} />
+          </div>
+          <p className='money'><b></b></p>
+          <button className='addCart'><span className="material-symbols-outlined"></span>장바구니</button>
+        </article>
+        </>
       ) : error ? (
         <h1>error</h1>
       ) : (
@@ -27,13 +60,17 @@ const Details = () => {
           </figure>
         </aside>
         <article>
+          <p className='type'>{nameData.category[type]}</p>
           <p className='itemName'>{data[0].nameKR}</p>
           <i>{capitalize(data[0].name)}</i>
-          <p className='type'>{nameData.category[type]}</p>
+          <q>{data[0].catchphrases}</q>
           <p className='money'><b>{data[0].sell_nook.toLocaleString('en-US')}</b>벨</p>
-          <p className='shipping'>택배배송 | 배송비 3,000원 (4만원 이상 무료) | 너굴 택배</p>
-          <p className='money'><b></b>벨</p>
-          <button><span className="material-symbols-outlined"></span>장바구니</button>
+          <p className='shippingDesc'>택배배송 | 배송비 1,200벨 (10,000벨 이상 무료) | 너굴 택배</p>
+          <div className='option'>
+            <CountButton className="cntbutton" numstate={[num, setNum]} inputref={inputref} />
+          </div>
+          <p className='money'>총 {num}개  |  <b>{(num*data[0].sell_nook).toLocaleString('en-US')}</b>벨</p>
+          <button className='addCart' onClick={handleClick}><span className="material-symbols-outlined"></span>장바구니</button>
         </article>
         <main></main>
         </>
