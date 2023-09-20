b39092be4555cb7574c813fd2176257ed268f291
pepprbell <pepprbell@gmail.com>
2023-09-21 00:35:57 +0900

    Fix highlighting logic: now it highlights on detail page

--- 5.React/odin-shopping-cart/src/components/CustomLink.jsx
+++ 5.React/odin-shopping-cart/src/components/CustomLink.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 const CustomLink = ({ category, className }) => {
   const location = useLocation()
 
-  const here = location.pathname === `/${category[0]}`
+  const here = location.pathname.includes(`/${category[0]}`)
 
   return <Link to={category[0]} className={here ? className + ' bold' : className} key={category[0]} >{category[1]}</Link>
 }
