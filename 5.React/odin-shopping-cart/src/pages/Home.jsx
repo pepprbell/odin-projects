111989db65916e6e9b12969d4f027e563932e152
pepprbell <pepprbell@gmail.com>
2023-09-26 17:34:37 +0900

    Add banner for mobile view

--- 5.React/odin-shopping-cart/src/pages/Home.jsx
+++ 5.React/odin-shopping-cart/src/pages/Home.jsx
@@ -5,13 +5,40 @@ import bugs from '../assets/Bug_NH_Icon.png'
 import sea from '../assets/Sea_Creature_NH_Icon.png'
 import { Link } from 'react-router-dom';
 import dayBanner from '../assets/banner_day.png'
+import dayBannerM from '../assets/banner_day_m.png'
 import nightBanner from '../assets/banner_night.png'
+import nightBannerM from '../assets/banner_night_m.png'
+import { useEffect, useState } from 'react';
 
 const Home = () => {
   const now = new Date().getHours()
-  console.log(now)
   const to = 6 <= now && now < 18 ? '/fish/Guppy' : '/fish/Barreleye'
-  const src = 6 <= now && now < 18 ? dayBanner : nightBanner
+  const [src, setSrc] = useState(dayBanner)
+
+  useEffect(() => {
+    getSrc()
+
+    window.addEventListener('resize', getSrc)
+    return () => window.removeEventListener('resize', getSrc)
+  }, [])
+
+  function getSrc() {
+    if (6 <= now && now < 18) {
+      // day
+      if (window.innerWidth > 1000) {
+        setSrc(dayBanner)
+      } else {
+        setSrc(dayBannerM)
+      }
+    } else {
+      // night
+      if (window.innerWidth > 1000) {
+        setSrc(nightBanner)
+      } else {
+        setSrc(nightBannerM)
+      }
+    }
+  }
 
   return (
     <section className='home'>
